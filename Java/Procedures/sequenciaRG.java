151c44e24a50fd8bdd4e734bd5b2d10749f998ab
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2019-11-15 13:40:44 -0300

    Ex. Maratone

---
+++ Java/Procedures/sequenciaRG.java
@@ -0,0 +1,58 @@
+package quadmagic;
+
+import java.util.Scanner;
+
+public class sequenciaRG {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            int size = scan.nextInt();
+            if (size == 0) {
+                break;
+            }
+            int[] sequence = new int[size];
+
+            for (int i = 0; i < sequence.length; i++) {
+                sequence[i] = scan.nextInt();
+            }
+            if (checkSequenceRG(sequence, size, 0) > 0) {
+                System.out.println("Sequence-RG.");
+
+            } else {
+                System.out.println("Not a Sequence-RG.");
+            }
+
+        }
+
+    }
+
+    static int checkSequenceRG(int sequence[], int terms, int index) {
+        if (terms == 0) {
+            return 0;
+        }
+
+        if (index == terms - 1) {
+            return -1;
+        }
+
+        int result = sequence[index] - sequence[index + 1];
+        if (result < 0) {
+            result = result * -1;
+        }
+        int next = checkSequenceRG(sequence, terms, index + 1);
+
+        if (next == -1) {
+            if(result != 1)
+            {
+                return -2;
+            }
+            return result;
+        }
+
+        if (result - 1 != next) {
+            return -2;
+        }
+        return result;
+    }
+}
