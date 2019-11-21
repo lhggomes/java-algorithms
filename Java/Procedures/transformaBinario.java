8e34ff695ae2831fdd1055a9e8fc9cd87a2f0032
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2019-11-20 22:16:30 -0300

    projeto integrador

---
+++ Java/Procedures/transformaBinario.java
@@ -0,0 +1,48 @@
+package aulas.projetoIntegrador;
+public class transformaBinario {
+
+    public int converteBinario(String number) {
+        String numberToConvert = number;
+        
+          //Separating the numbers
+          
+        int[] array = new int[100];
+        for (int i = 0; i < numberToConvert.length(); i++) {
+            array[i] = numberToConvert.charAt(i) - 48;
+        }
+        int auxNumber = numberToConvert.length() - 1;
+        int numberDecrememt = auxNumber;
+        int sumNumber = 0;
+
+        for (int i = 0; i <= auxNumber; i++) {
+            sumNumber += array[i] * Math.pow(2, numberDecrememt);
+            numberDecrememt--;
+        }
+        return sumNumber;
+
+    }
+
+}
+
+/* 
+Scanner scan = new Scanner(System.in);
+        String numberToConvert = "10101";
+
+        //Separating the numbers
+        int[] array = new int[100];
+        for (int i = 0; i < numberToConvert.length(); i++) {
+            array[i] = numberToConvert.charAt(i)-48;
+        }
+
+        int auxNumber = numberToConvert.length() -1 ;
+        int numberDecrememt = auxNumber;
+        int sumNumber = 0;
+
+        for (int i = 0; i <= auxNumber; i++) {
+            sumNumber += array[i]* Math.pow(2, numberDecrememt);
+            numberDecrememt--;
+
+        }
+        
+        System.out.println(sumNumber);
+ */
